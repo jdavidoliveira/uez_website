f7346f3d9b2514bcc15c3ea63aa475829d655b63
Renato Gomes do Nascimento <110681726+RENATO-GDN@users.noreply.github.com>
2023-11-10 13:01:17 -0300

    ...

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -22,8 +22,13 @@ export default function PagesLayout({
     <>
       <Header />
       {children}
-      <Link title="SAC" href="https://api.whatsapp.com/send?phone=5521982964498&text=SERVI%C3%87O%20DE%20ATENDIMENTO%20DA%20UEZCOMPANY" target='_blank' className='fixed bottom-8 bg-emerald-500 right-4 rounded-full w-14 h-14 flex items-center justify-center hover:scale-105 transition-transform cursor-pointer animate-pulse'>
-        <Phone color='white' size={30} />
+      <Link
+        title="SAC"
+        href="https://api.whatsapp.com/send?phone=5521982964498&text=Olá%2C%20gostaria%20de%20saber%20mais%20sobre%20como%20funciona%20a%20UEZ"
+        target='_blank'
+        className='fixed sm:bottom-8 bottom-6 bg-emerald-500 right-4 rounded-full sm:w-14 sm:h-14 w-10 h-10 flex items-center justify-center hover:scale-105 transition-transform cursor-pointer animate-pulse'
+      >
+        <Phone color='white' size={30} className='w-1/2 sm:w-8/12' />
       </Link>
       <Footer />
     </>
