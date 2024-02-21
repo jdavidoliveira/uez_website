f1e192486863fbc4809bec042ed1dfa11eb5dfc7
lordaval <lordaval1117@gmail.com>
2024-02-21 17:57:26 -0300

    ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,33 @@
-import './globals.css'
-import Providers from '@/contexts/Providers'
-import { Montserrat } from 'next/font/google'
-import type { Metadata } from 'next'
-import { Analytics } from '@vercel/analytics/react'
+import "./globals.css";
+import Providers from "@/contexts/Providers";
+import { Montserrat } from "next/font/google";
+import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | UEZ Company',
-    default: 'UEZ Company',
+    template: "%s | UEZ Company",
+    default: "UEZ Company",
   },
-  description: 'Surgiu algum problema e você precisa de ajuda? Então bem-vindo á Uez!',
-  keywords: 'UEZ, Company, UEZ Company, UEZ Company, UEZ Company, serviços, freelance',
-  themeColor: '#fff',
-}
+  description:
+    "Surgiu algum problema e você precisa de ajuda? Então bem-vindo á Uez!",
+  keywords:
+    "UEZ, Company, UEZ Company, UEZ Company, UEZ Company, serviços, freelance",
+  themeColor: "#fff",
+};
 
 const montserrat = Montserrat({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  style: ['italic', 'normal'],
-  subsets: ['latin'],
-  variable: '--font-montserrat',
-})
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["italic", "normal"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
@@ -32,8 +35,9 @@ export default function RootLayout({
         <body className={montserrat.className} suppressHydrationWarning={true}>
           {children}
           <Analytics />
+          <SpeedInsights />
         </body>
       </Providers>
     </html>
-  )
+  );
 }
