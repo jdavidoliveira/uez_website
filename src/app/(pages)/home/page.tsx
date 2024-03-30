54509a60c6fddfccd67c6e16225e5a039b330a97
Danilo <htpps.sw@gmail.com>
2024-03-30 01:14:03 -0300

    create p0 p2 p3 p4 p6

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -6,97 +6,109 @@ import Link from "next/link"
 import "animate.css/animate.min.css"
 import { AnimationOnScroll } from "react-animation-on-scroll"
 
-export default function Home() {
+export default function Home() { 
   return (
-    <main className="w-full flex flex-col items-center mx-0 bg-white" id="inicio">
-      <ImageSection imageUrl="/images/p1.jpg">
-        <div className="px-6 py-12 bg-azulao absolute right-0 top-1/3 rounded-l-full flex items-center justify-center">
-          <h1 className="mx-8 text-white text-left text-4xl font-semibold">
-            Encontre uzers <br /> que possam te ajudar!
-          </h1>
+    <main className="w-full flex flex-col items-center mx-0 gap-16 bg-white" id="inicio">
+      
+      {/* P0 - FEITO */}
+      {/* <ImageSection imageUrl="/images/p0.jpg">
+        <div className="flex flex-col z-10 absolute right-1/2 translate-x-1/2 mt-40 gap-8 items-center">
+          <Image src="/images/logo.png" alt={"Logo da UEZ"} width={250} height={200}/>
+          <div className="text-center">
+            <h1 className="text-3xl"> Porque a vida é mais fácil </h1>
+            <h1 className="text-3xl"> 
+            quando estamos 
+            <span className="">conectados</span>
+            </h1>
+          </div>
         </div>
-        <button className="bg-azulao py-1 px-4 rounded-full font-bold text-white absolute bottom-6 left-1/2 -translate-x-1/2 transition hover:scale-105">
-          <Link href="/cadastro?userType=cliente">Torne-se um Cliente</Link>
-        </button>
-        <div className="w-40 h-40 bg-azulao absolute bottom-0 left-0 -translate-x-1/2 rounded-r-full smmobile:w-28 smmobile:h-28"></div>
-      </ImageSection>
-      <ImageSection imageUrl="/images/p2.jpg">
-        <div className="px-6 py-12 bg-roxazul absolute left-0 top-1/3 rounded-r-full flex items-center justify-center">
-          <h1 className="mx-8 text-white text-left text-4xl font-semibold">
-            Consiga clientes, <br /> ajude mais pessoas!
-          </h1>
-        </div>
-        <button className="bg-white py-1 px-4 rounded-full font-bold text-azulao absolute bottom-6 left-1/2 -translate-x-1/2 transition hover:scale-105">
-          <Link href="/cadastro?userType=uzer">Torne-se um Uzer</Link>
-        </button>
-        <div className="w-20 h-40 bg-roxazul absolute bottom-0 right-0 translate-y-1/2 rounded-l-full smmobile:h-28 smmobile:w-[3.5rem]"></div>
-      </ImageSection>
+      </ImageSection> */}
+ 
+      {/* P1 */} 
 
-      <GenericSection className="relative flex flex-col items-center justify-around gap-8 text-azulao mb-28">
-        <div className="w-40 h-40 bg-roxazul absolute top-1/4 left-0 -translate-x-1/2 rounded-r-full smmobile:w-28 smmobile:h-28"></div>
-        <div className="w-20 h-40 bg-roxazul absolute top-3/4 right-0 rounded-l-full smmobile:h-28 smmobile:w-[3.5rem]"></div>
-        <div className="w-40 h-40 bg-roxazul absolute bottom-0 left-0 -translate-x-1/2 translate-y-1/2 rounded-r-full smmobile:w-28 smmobile:h-28"></div>
-        <div className="relative flex flex-col items-center justify-around gap-16 mb-16 text-azulao w-11/12">
+
+      {/* P2 - FEITO */}
+      {/* <ImageSection imageUrl="/images/P2.jpg">
+        <div className="absolute w-full flex flex-col z-10 bottom-0 gap-44 left-0 mb-8">
+          <h1 className=" w-[430px] py-8 pr-4 bg-white text-4xl font-medium text-right rounded-r-2xl " >
+            Consiga clientes  <br />
+            feche <span className="text-azulao">mais</span> negócios 
+          </h1>
+          <h1 className="p-4 cursor-pointer bg-white text-4xl font-medium text-center flex self-center rounded-2xl transition duration-100 hover:animate-none hover:duration-100 hover:scale-105 animate-pulse">
+            Cadastrar como uzer 
+          </h1>
+        </div>
+      </ImageSection> */}
+
+      {/* P3 - FEITO */}
+      {/* <GenericSection className="relativa h-144  flex flex-col items-center justify-around bg-pink text-azulao" >
+        <div className=" flex flex-col items-center justify-around gap-16 text-azulao ">
           <AnimationOnScroll animateIn="animate__fadeInUp" animateOnce>
-            <h1 className="text-4xl font-semibold text-center">O que é a UEZ Company?</h1>
+            <h1 className="text-4xl font-semibold text-center">O que é a UEZ?</h1>
           </AnimationOnScroll>
           <AnimationOnScroll animateIn="animate__fadeInUp" animateOnce>
             <p className="text-lg leading-snug font-medium text-center mx-auto w-10/12">
-              A <strong>UEZ Company</strong> é uma empresa que tem o objetivo de facilitar a sua vida, te ajudando a
-              encontrar profissionais que possam realizar algum serviço para você ou se você é esse profissional e está
-              em busca de novos clientes.
+              A <strong> UEZ </strong> é um software que tem o objetivo de facilitar a sua vida, te ajudando a encontrar profissionais que possam realizar algum serviço online para você. Além disso, se você sabe fazer algo que se enquadre nos nossos serviços, nós te ajudamos a encontrar novos clientes.
             </p>
           </AnimationOnScroll>
         </div>
-        <div className="relative flex flex-col items-center justify-around gap-8 text-azulao w-11/12">
-          <AnimationOnScroll animateIn="animate__fadeInUp" animateOnce>
-            <h1 className="text-4xl font-semibold text-center mb-16 ">Como funciona?</h1>
-          </AnimationOnScroll>
-          <div className="flex flex-col gap-4 items-center w-full">
-            <div className={`flex gap-4 items-center justify-around w-full`}>
-              <CardPhoto
-                imageUrl="/images/cliente.png"
-                title="Cliente"
-                description="Uma pessoa que precisa de um serviço, por exemplo, uma pessoa com a tubulação quebrada."
-              />
-              <CardPhoto
-                imageUrl="/images/uzer.png"
-                title="Uzer"
-                description="Um profissional que precisa de clientes, por exemplo, um encanador capaz de consertar a tubulação."
-              />
-            </div>
-            <div className="flex flex-col gap-4 items-center justify-center w-full">
-              <Image src="/vetores/linha-baixo.svg" width={120} height={120} alt="Vetor de linhas" className="w-6/12" />
-              <div className="w-40 h-40 transition hover:scale-105">
-                <a href="#inicio" title="Uez Company">
-                  <Image
-                    src="logo.svg"
-                    width={120}
-                    height={120}
-                    alt="Logo da UEZ Company"
-                    className=" animate-pulse w-40 h-40 transition hover:animate-none hover:duration-300 hover:scale-105"
-                  />
-                </a>
-              </div>
-              <Image src="/vetores/linha-cima.svg" width={120} height={120} alt="Vetor de linhas" className="w-6/12" />
-            </div>
-            <div className="flex gap-4 items-center justify-around w-full">
-              <CardPhoto
-                imageUrl="/images/batepapo.png"
-                title="Chat"
-                description="Aqui o cliente e o uzer conversam para decidirem o preço, enviar especificações e para montar o orçamento."
-              />
-              <CardPhoto
-                imageUrl="/images/riquinho.png"
-                title="Orçamento"
-                description="Quando o orçamento é lançado para o cliente, ele tem a opção de aceitar ou não, priorizando a segurança."
-              />
-            </div>
-          </div>
-        </div>
-      </GenericSection>
+      </GenericSection> */}
 
-      <GenericSection className="relative flex flex-col items-center justify-around gap-8 text-azulao mb-20">
+      {/* P4 */}
+      {/* <ImageSection imageUrl="/images/p4.jpg">
+        <div className="flex flex-col items-center justify-around gap-8 text-azulao ">
+            <AnimationOnScroll animateIn="animate__fadeInUp" animateOnce>
+              <h1 className="text-4xl font-semibold text-center mb-16 ">Como funciona?</h1>
+            </AnimationOnScroll>
+            <div className="flex flex-col gap-4 items-center w-full">
+              <div className={`flex gap-4 items-center justify-around w-full`}>
+                <CardPhoto
+                  imageUrl="/images/cliente.png"
+                  title="Cliente"
+                  description="Uma pessoa que precisa de um serviço, por exemplo, uma pessoa com a tubulação quebrada."
+                />
+                <CardPhoto
+                  imageUrl="/images/uzer.png"
+                  title="Uzer"
+                  description="Um profissional que precisa de clientes, por exemplo, um encanador capaz de consertar a tubulação."
+                />
+              </div>
+              <div className="flex flex-col gap-4 items-center justify-center w-full">
+                <Image src="/vetores/linha-baixo.svg" width={120} height={120} alt="Vetor de linhas" className="w-6/12" />
+                <div className="w-40 h-40 transition hover:scale-105">
+                  <a href="#inicio" title="Uez Company">
+                    <Image
+                      src="logo.svg"
+                      width={120}
+                      height={120}
+                      alt="Logo da UEZ Company"
+                      className=" animate-pulse w-40 h-40 transition hover:animate-none hover:duration-300 hover:scale-105"
+                    />
+                  </a>
+                </div>
+                <Image src="/vetores/linha-cima.svg" width={120} height={120} alt="Vetor de linhas" className="w-6/12" />
+              </div>
+              <div className="flex gap-4 items-center justify-around w-full">
+                <CardPhoto
+                  imageUrl="/images/batepapo.png"
+                  title="Chat"
+                  description="Aqui o cliente e o uzer conversam para decidirem o preço, enviar especificações e para montar o orçamento."
+                />
+                <CardPhoto
+                  imageUrl="/images/riquinho.png"
+                  title="Orçamento"
+                  description="Quando o orçamento é lançado para o cliente, ele tem a opção de aceitar ou não, priorizando a segurança."
+                />
+              </div>
+            </div>
+        </div>
+      </ImageSection> */}
+  
+      {/* P5 */}
+
+
+      {/* P6 - FEITO */}
+      {/* <GenericSection className="pt-16 pb-8 relative flex flex-col items-center justify-around gap-8 text-azulao mb-20">
         <div className="relative flex flex-col items-center justify-around gap-8 text-azulao w-11/12">
           <Image
             src="/images/icons/cadeado.svg"
@@ -113,22 +125,16 @@ export default function Home() {
               O nosso modo de pagamento zela tanto pela segurança dos Clientes quanto dos Uzers.
             </p>
           </AnimationOnScroll>
+
+          <Image
+            src="/images/icons/cadeado.svg"
+            width={120}
+            height={120}
+            alt="Imagem ilustrativa de um cadeado"
+            className="w-12 h-12 absolute top-8 right-0 mobile:hidden animate-pulse"
+          />
         </div>
         <div className="relative flex flex-col items-center justify-around gap-8 text-azulao w-11/12">
-          <Image
-            src="/images/icons/cadeado.svg"
-            width={120}
-            height={120}
-            alt="Imagem ilustrativa de um cadeado"
-            className="sm:hidden w-12 h-12 absolute top-0 left-0 smmobile:hidden animate-pulse"
-          />
-          <Image
-            src="/images/icons/cadeado.svg"
-            width={120}
-            height={120}
-            alt="Imagem ilustrativa de um cadeado"
-            className="w-12 h-12 absolute top-0 right-0 smmobile:hidden animate-pulse"
-          />
           <AnimationOnScroll animateIn="animate__fadeInUp" animateOnce>
             <h1 className="text-4xl font-bold text-center">Mas de que forma?</h1>
           </AnimationOnScroll>
@@ -140,25 +146,29 @@ export default function Home() {
             </p>
           </AnimationOnScroll>
         </div>
-      </GenericSection>
+      </GenericSection>  */}
     </main>
   )
 }
 
-const GenericSection = ({ className = "", children }: { className: string; children: ReactNode }) => (
+const GenericSection = ({ className = "", children }: { className: string; children: ReactNode }) =>  (
   <section
-    className={`w-full px-0 py-16 flex flex-row items-center justify-around gap-8 text-azulao animate-transitionY ${className}`}
+    className={`w-full  px-0 flex flex-row items-center justify-around  text-azulao animate-transitionY ${className}`}
   >
     {children}
   </section>
 )
 
 const ImageSection = ({ imageUrl, children }: { imageUrl: StaticImageData | string; children: ReactNode }) => (
-  <section className={`w-full h-144 p-0 m-0 bg-gray-600 bg-center bg-cover bg-no-repeat aspect-video relative`}>
-    <Image src={imageUrl} alt="Imagem de fundo" fill className="object-cover object-center" priority />
+  <section className={`w-full p-0 m-0 bg-center bg-cover bg-no-repeat aspect-video relative`}>
+    <Image src={imageUrl} alt="Imagem de fundo" fill className="object-cover object-center " priority />
     {children}
   </section>
 )
+
+
+
+
 
 const CardPhoto = ({ imageUrl, title, description }: { imageUrl: string; title: string; description: string }) => (
   <div className="w-8/12 flex flex-col items-center justify-between gap-2 transition duration-300 hover:animate-none hover:duration-300 hover:scale-105 animate-pulse">
