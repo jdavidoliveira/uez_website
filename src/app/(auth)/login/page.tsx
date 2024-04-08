b9fb02990f24d45ad18281f6d09402209e7f239c
Danilo <htpps.sw@gamil.com>
2024-04-08 15:11:12 -0300

    update login screen

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,7 +4,8 @@ import Modal from '@/components/Modal/Modal'
 import Input from '@/components/Forms/Input/Input'
 import { EyeClosedIcon, EyeOpenIcon } from '@radix-ui/react-icons'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, } from 'react'
+import Image from "next/image"
 import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -13,9 +14,7 @@ import LoadingSpinner from '@/components/LoadingSpinner/LoadingSpinner'
 import { useRouter, useSearchParams } from 'next/navigation'
 
 const userFormSchema = z.object({
-  email: z.string()
-    .nonempty("O e-mail é obrigatório")
-    .email('Formato de e-mail inválido'),
+  name: z.string().min(1, "Um nome deve ser informado").min(3, "O nome indicado deve ser valido"),
   senha: z.string()
     .nonempty("A senha é obrigatória")
     .min(6, "A senha deve ter mais de 6 caracteres")
@@ -45,9 +44,9 @@ export default function Login() {
     setShowModal(prevState => !prevState)
   }
 
-  async function logar({ email, senha }: { email: string, senha: string }) {
+  async function logar({ name, senha }: { name: string, senha: string }) {
     setIsSubmitting(true)
-    const logError = await login(email, senha)
+    const logError = await login(name, senha)
     if (logError) {
       toggleModal(logError)
       setIsSubmitting(false)
@@ -66,82 +65,98 @@ export default function Login() {
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   return (
-    <form className="bg-white rounded-3xl p-3 max-h-full w-[30%] flex flex-col items-center justify-center font-Montserrat mobile:w-full mobile:h-full mobile:rounded-none mobile:justify-start" onSubmit={handleSubmit(logar)}>
-      <div className="w-4/5 h-full flex flex-col items-center justify-between gap-4 my-4 mx-auto mobile:h-4/5 mobile:max-h-96">
-        <h1 className="font-extrabold text-3xl my-2">Login</h1>
-        <div className="flex flex-col items-center justify-center w-full">
-          <label htmlFor="email" title="E-mail" className="self-start text-base font-medium">
-            E-mail:
-          </label>
-          <div className="flex items-center w-full h-10">
-            <input
-              className={`bg-cinzero w-full h-10 font-medium text-base px-3 py-2 outline-none ${errors.email && "border-2 rounded border-red-500"}`}
-              type="text"
-              id="email"
-              maxLength={200}
-              defaultValue={searchParams.get("userEmail") ?? ""}
-              placeholder="example@gmail.com"
-              {...register("email")}
-            />
+    <form className="bg-white rounded-3xl p-3 max-h-full w-[35%] flex flex-col items-center h-[600px] justify-between font-Montserrat mobile:w-full mobile:h-full mobile:rounded-none mobile:justify-start " onSubmit={handleSubmit(logar)}>
+      <legend className='font-extrabold text-2xl mt-4'>
+        Login
+      </legend>
+
+     <div className='border border-x-0 border-t-0 border-b-gray-400 w-full h-[30%] flex flex-col justify-between items-center'>
+      <div> ㅤ</div>
+
+      <h1 className='px-10 p-1 shadow-xl  shadow-slate-300 rounded-xl border items-center border-slate-200 gap-4 flex hover:border-slate-400 hover:rounded-3xl duration-1000 cursor-pointer hover:shadow-2xl hover:shadow-slate-400'>
+          <Image width={20} height={20} alt='logo da Goole' src="/google.svg" className='flex items-center mt-[6px]'/>
+          Entrar com o google
+      </h1>
+
+      <div> ㅤ</div>
+      <span className='-m-5 bg-white p-2'>OU</span>
+     </div>
+  
+
+      <div className='gap-5 flex flex-col w-[90%]'>
+        <div className="flex flex-col items-center justify-center w-full ">
+            <label htmlFor="email" title="E-mail" className="self-start text-base font-medium">
+              Nome de usuário 
+            </label>
+            <div className="flex items-center w-full h-10">
+              <input
+                className={`bg-cinzero w-full h-10 font-medium text-base px-3 py-2 outline-none rounded-lg`}
+                type="text"
+                id="nameUser"
+                maxLength={200}
+                
+                {...register("name")}
+              />
+            </div>
+            {errors.name && <span className="font-medium text-xs self-start my-1">{errors.name.message}</span>}
           </div>
-          {errors.email && <span className="font-medium text-xs self-start my-1">{errors.email.message}</span>}
-        </div>
-        <div className="flex flex-col items-center justify-center w-full">
-          <label htmlFor="senha" title="Senha" className="self-start text-base font-medium">
-            Senha:
-          </label>
-          <div className="flex items-center w-full h-10">
-            <input
-              className={`bg-cinzero w-full h-10 font-medium text-base px-3 py-2 outline-none ${errors.senha && "border-2 rounded border-red-500"}`}
-              type={passwordType}
-              id="senha"
-              maxLength={24}
-              {...register("senha", {
-                onChange: (e) => {
-                  if (e.target.value.length > 0) {
-                    setShowPasswordChanger(true)
-                  } else {
-                    setShowPasswordChanger(false)
+
+          <div className="flex flex-col items-center justify-center w-full">
+            <label htmlFor="senha" title="Senha" className="self-start text-base font-medium">
+              Senha:
+            </label>
+            <div className="flex items-center w-full h-10">
+              <input
+                className={`bg-cinzero w-full h-10 font-medium rounded-lg text-base px-3 py-2 outline-none ${errors.senha && "border-2 rounded border-red-500"}`}
+                type={passwordType}
+                id="senha"
+                maxLength={24}
+                {...register("senha", {
+                  onChange: (e) => {
+                    if (e.target.value.length > 0) {
+                      setShowPasswordChanger(true)
+                    } else {
+                      setShowPasswordChanger(false)
+                    }
                   }
-                }
-              })}
+                })}
 
-            />
-            {showPasswordChanger && <button
-              title="Exibir/ocultar senha"
-              type="button"
-              className="bg-cinzero hover:bg-[#e9e9e9] border-none py-2 px-3 h-full cursor-pointer flex items-center justify-center"
-              onClick={(e) => {
-                e.preventDefault();
-                setPasswordType(prevState => {
-                  setPwChangerIcon(prevState === "text" ? <EyeClosedIcon width={20} height={20} /> : <EyeOpenIcon width={20} height={20} />)
-                  return prevState === "text" ? "password" : "text"
-                })
+              />
+              {showPasswordChanger && <button
+                title="Exibir/ocultar senha"
+                type="button"
+                className="bg-cinzero hover:bg-[#e9e9e9] border-none py-2 px-3 h-full cursor-pointer flex items-center justify-center"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setPasswordType(prevState => {
+                    setPwChangerIcon(prevState === "text" ? <EyeClosedIcon width={20} height={20} /> : <EyeOpenIcon width={20} height={20} />)
+                    return prevState === "text" ? "password" : "text"
+                  })
 
-              }}
-            >
-              {pwChangerIcon}
-            </button>}
+                }}
+              >
+                {pwChangerIcon}
+              </button>}
+            </div>
+            {errors.senha && <span className="font-medium text-xs self-start my-1">{errors.senha.message}</span>}
           </div>
-          {errors.senha && <span className="font-medium text-xs self-start my-1">{errors.senha.message}</span>}
-        </div>
-        <div className="w-full flex items-center justify-between">
-          <span className="flex items-center gap-1">
-            <Input type="checkbox" id="manterlogin" className="w-4 h-4 border border-black bg-white" noLabel />
-            <label htmlFor="manterlogin" className="font-bold text-sm">Manter-se conectado</label>
-          </span>
-          <span className="flex items-center justify-center"><Link href="/esqueci-senha" className="font-extrabold text-xs text-azulao italic hover:underline">Esqueci minha senha</Link></span>
-        </div>
-        <button type="submit" className="bg-azulao border-none flex items-center justify-center py-2 px-4 rounded-lg text-white text-xl font-extrabold hover:bg-[#0f0f5c] w-full">
-          {isSubmitting ? <LoadingSpinner size={10} /> : "Entrar"}
-        </button>
-        <div className="w-full flex items-center justify-between">
-          <span className="flex items-center justify-center mx-4">
-            <p className="font-medium text-sm">Não tem uma conta? <Link href="/cadastro" className="text-[#5e5bff] hover:underline">Cadastre-se</Link> </p>
-          </span>
+
+          <div className="w-full flex items-center justify-between">
+            <span className="flex items-center gap-1">
+              <Input type="checkbox" id="manterlogin" className="w-4 h-4 border border-black bg-white" noLabel />
+              <label htmlFor="manterlogin" className="font-bold text-sm">Manter-se conectado</label>
+            </span>
+            <span className="flex items-center justify-center"><Link href="/esqueci-senha" className="font-extrabold text-xs text-azulao italic hover:underline">Esqueci minha senha</Link></span>
+          </div>
+          <button type="submit" className="bg-azulao border-none flex items-center justify-center py-2 px-4 rounded-lg text-white text-xl font-extrabold hover:bg-[#0f0f5c] w-full">
+            {isSubmitting ? <LoadingSpinner size={10} /> : "Entrar"}
+          </button>
+          <div className="w-full flex items-center justify-between">
+            <span className="flex items-center justify-center mx-4">
+              <p className="font-medium text-sm">Não tem uma conta? <Link href="/cadastro" className="text-[#5e5bff] hover:underline">Cadastre-se</Link> </p>
+            </span>
         </div>
       </div>
-      {showModal && <Modal message={modalMessage} handleClick={() => setShowModal(false)} noButton={!haveButton} />}
     </form>
   )
 }
